84360bf20fd760c9141bf730c590f04b24653dc5
Jeffrey Skinner <17016344+blocksrocks@users.noreply.github.com>
2020-03-09 14:52:41 -0700

    error fix

--- capture.lua
+++ capture.lua
@@ -93,9 +93,9 @@ structure.MeshPart = structure.WedgePart
 
 structure.light = function(o, m)
 	if o:IsA("BasePart") then
-		local pos = argstr("vec3.new", ", ", ")", v3unp(o.CFrame.Position))
-		local siz = argstr("vec3.new", ", ", ")", v3unp(o.Size))
-		local col = argstr("vec3.new", ", ", ")", c3unp(o.Color))
+		local pos = argstr("vec3.new(", ", ", ")", v3unp(o.CFrame.Position))
+		local siz = argstr("vec3.new(", ", ", ")", v3unp(o.Size))
+		local col = argstr("vec3.new(", ", ", ")", c3unp(o.Color))
 		local opa = 1 - o.Transparency
 		return argstr("{", ", ", "};", pos, siz, col, opa)
 	end
